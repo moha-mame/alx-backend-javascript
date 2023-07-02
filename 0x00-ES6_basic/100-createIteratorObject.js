24aee8df8d47dcf91cfccf808d670859fb542a18
moha-mame <moha.mame04@gmail.com>
2023-07-02 16:26:28 +0000

    ES6_basic

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,30 @@
+export default function createIteratorObject(report) {
+  let currentDepartmentIndex = 0;
+  let currentEmployeeIndex = 0;
+
+  const iterator = {
+    next() {
+      const departments = Object.keys(report.allEmployees);
+
+      if (currentDepartmentIndex >= departments.length) {
+        return { done: true };
+      }
+
+      const currentDepartment = departments[currentDepartmentIndex];
+      const employees = report.allEmployees[currentDepartment];
+
+      if (currentEmployeeIndex >= employees.length) {
+        currentDepartmentIndex++;
+        currentEmployeeIndex = 0;
+        return this.next();
+      }
+
+      const currentEmployee = employees[currentEmployeeIndex];
+      currentEmployeeIndex++;
+
+      return { value: currentEmployee, done: false };
+    }
+  };
+
+  return iterator;
+}
