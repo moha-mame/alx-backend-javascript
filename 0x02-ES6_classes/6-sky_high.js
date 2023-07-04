645d6a8e45d64dc06982ab50f2a89289e636953c
mohammed Abate <71933125+moha-mame@users.noreply.github.com>
2023-07-04 13:45:21 +0100

    Update 6-sky_high.js

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,19 +1,20 @@
+import Building from './5-building';
+
 export default class SkyHighBuilding extends Building {
-  constructor(sqft, floors) {
-    super(sqft);
+  constructor(sqrt, floors) {
+    super(sqrt);
     this._floors = floors;
   }
 
   set floors(floors) {
     this._floors = floors;
   }
-  
-  
+
   get floors() {
     return this._floors;
   }
 
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this._floors} floors.`;
+    return `Evacuate slowly the ${this.floors} floors`;
   }
 }
