5b3e4d94362a18cf12e8e51290cc21e14a9077c9
mohammed Abate <71933125+moha-mame@users.noreply.github.com>
2023-07-04 07:46:46 +0100

    Update 4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,8 +1,8 @@
 function updateStudentGradeByCity(students, city, newGrades) {
   return students
-    .filter(student => student.location === city)
-    .map(student => {
-      const foundGrade = newGrades.find(grade => grade.studentId === student.id);
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const foundGrade = newGrades.find((grade) => grade.studentId === student.id);
       const grade = foundGrade ? foundGrade.grade : 'N/A';
       return { ...student, grade };
     });
